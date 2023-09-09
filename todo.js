58ee26dadf90c75627803532d86c0ccb2e23050c
SaarthakGitHub <91599391+SaarthakGitHub@users.noreply.github.com>
2023-09-09 15:01:56 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -7,7 +7,7 @@ const port = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
-mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://saarthak173:Test123@cluster0.fsiscpa.mongodb.net/todolistDB", {useNewUrlParser: true});
  const itemsSchema = new mongoose.Schema(
     {
         name: String
